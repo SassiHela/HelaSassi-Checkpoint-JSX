6b05c5ea9097452d9a16f54a9234d26d0083f35c
Hela Sassi <hela.sassi13@gmail.com>
2020-10-03 22:06:42 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./style/style.css";
+import "./App.css";
+import imgSrc from "./assets/imageInSrc.jpg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div
+        style={{
+          border: "solid 1px black",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <h1 className="title red" style={{ marginBottom: 0 }}>
+          Hela Sassi
+        </h1>
+
+        <br></br>
+
+        <img src={imgSrc} style={{ height: 500, width: 350 }} alt="natureImg" />
+
+        <br></br>
+
+        <img
+          src={"../images/imageInPublic.jpg"}
+          style={{ height: 500, width: 350 }}
+          alt="seaImg"
+        />
+
+        <video style={{ width: 320, height: 240, margin: 15 }} controls>
+          <source src="myVideo.mp4" type="video/mp4" />
+        </video>
+      </div>
     </div>
   );
 }
